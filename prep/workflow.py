008f8889a58c8fe078a3cd8af7aa39c573028f96
Matt Dupree <kmdupr33@gmail.com>
2023-02-03 17:47:24 -0500

    replace relative borked_iris referencd with url

--- prep/workflow.py
+++ prep/workflow.py
@@ -120,7 +120,7 @@ from sklearn.model_selection import train_test_split
 
 @dchimp.task
 def prep():
-    df = pd.read_csv('borked_iris.csv')
+    df = pd.read_csv('https://raw.githubusercontent.com/getdatachimp/demo/main/prep/borked_iris.csv')
     X = df[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
     y = df['variety'].map({'Setosa': 0, 'Versicolor': 1, 'Virginica': 2})
     return train_test_split(X, y, random_state=0)
