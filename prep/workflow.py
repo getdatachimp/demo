49d56cfe9e9c7fba698c95014761fe8e71cfa8a7
Matt Dupree <kmdupr33@gmail.com>
2023-02-03 22:59:17 -0500

    update wf

--- prep/workflow.py
+++ prep/workflow.py
@@ -1,126 +1,14 @@
 from data_chimp_executor import execute as dchimp
 import pandas as pd
-from sklearn.model_selection import train_test_split
 
 @dchimp.task
-def prep(X_train, X_test, y_train, y_test):
-    df = pd.read_csv('borked_iris.csv')
-    X = df[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
-    y = df['variety'].map({'Setosa': 0, 'Versicolor': 1, 'Virginica': 2})
-    return train_test_split(X, y, random_state=0)
-import pandas as pd
-from sklearn.model_selection import train_test_split
-
-@dchimp.task
-def prep(X_train, X_test, y_train, y_test):
-    df = pd.read_csv('borked_iris.csv')
-    X = df[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
-    y = df['variety'].map({'Setosa': 0, 'Versicolor': 1, 'Virginica': 2})
-    return train_test_split(X, y, random_state=0)
-import pandas as pd
-from sklearn.model_selection import train_test_split
-
-@dchimp.task
-def prep(X_train, X_test, y_train, y_test):
-    df = pd.read_csv('borked_iris.csv')
-    X = df[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
-    y = df['variety'].map({'Setosa': 0, 'Versicolor': 1, 'Virginica': 2})
-    return train_test_split(X, y, random_state=0)
-import pandas as pd
-from sklearn.model_selection import train_test_split
-
-@dchimp.task
-def prep():
-    df = pd.read_csv('borked_iris.csv')
-    X = df[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
-    y = df['variety'].map({'Setosa': 0, 'Versicolor': 1, 'Virginica': 2})
-    return train_test_split(X, y, random_state=0)
-import pandas as pd
-from sklearn.model_selection import train_test_split
-
-@dchimp.task
-def prep():
-    df = pd.read_csv('borked_iris.csv')
-    X = df[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
-    y = df['variety'].map({'Setosa': 0, 'Versicolor': 1, 'Virginica': 2})
-    return train_test_split(X, y, random_state=0)
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import make_pipeline
-
-@dchimp.task
-def train(X_train, y_train):
-    pipeline = make_pipeline(StandardScaler(), LogisticRegression())
-    return pipeline.fit(X_train, y_train)
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import make_pipeline
-
-@dchimp.task
-def train(X_train, y_train):
-    pipeline = make_pipeline(StandardScaler(), LogisticRegression())
-    return pipeline.fit(X_train, y_train)
-
-@dchimp.task
-def score(X_test, y_test, model):
-    return model.score(X_test, y_test)
-import pandas as pd
-from sklearn.model_selection import train_test_split
-
-@dchimp.task
-def prep():
-    df = pd.read_csv('borked_iris.csv')
-    X = df[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
-    y = df['variety'].map({'Setosa': 0, 'Versicolor': 1, 'Virginica': 2})
-    return train_test_split(X, y, random_state=0)
-import pandas as pd
-from sklearn.model_selection import train_test_split
-
-@dchimp.task
-def prep():
-    df = pd.read_csv('borked_iris.csv')
-    X = df[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
-    y = df['variety'].map({'Setosa': 0, 'Versicolor': 1, 'Virginica': 2})
-    return train_test_split(X, y, random_state=0)
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import make_pipeline
-
-@dchimp.task
-def train(X_train, y_train):
-    pipeline = make_pipeline(StandardScaler(), LogisticRegression())
-    return pipeline.fit(X_train, y_train)
-
-@dchimp.task
-def score(X_test, y_test, model):
-    return model.score(X_test, y_test)
-import pandas as pd
-from sklearn.model_selection import train_test_split
-
-@dchimp.task
-def prep():
-    df = pd.read_csv('borked_iris.csv')
-    X = df[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
-    y = df['variety'].map({'Setosa': 0, 'Versicolor': 1, 'Virginica': 2})
-    return train_test_split(X, y, random_state=0)
-from sklearn.linear_model import LogisticRegression
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import make_pipeline
-
-@dchimp.task
-def train(X_train, y_train):
-    pipeline = make_pipeline(StandardScaler(), LogisticRegression())
-    return pipeline.fit(X_train, y_train)
-
-@dchimp.task
-def score(X_test, y_test, model):
-    return model.score(X_test, y_test)
-import pandas as pd
-from sklearn.model_selection import train_test_split
-
-@dchimp.task
-def prep():
+def get_data():
     df = pd.read_csv('https://raw.githubusercontent.com/getdatachimp/demo/main/prep/borked_iris.csv')
+    return df
+from sklearn.model_selection import train_test_split
+
+@dchimp.task
+def prep(df):
     X = df[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
     y = df['variety'].map({'Setosa': 0, 'Versicolor': 1, 'Virginica': 2})
     return train_test_split(X, y, random_state=0)
@@ -129,7 +17,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
 
 @dchimp.task
-def trian(X_train, y_train):
+def train(X_train, y_train):
     pipeline = make_pipeline(StandardScaler(), LogisticRegression())
     return pipeline.fit(X_train, y_train)
 
--- prep/workflow_test.py
+++ prep/workflow_test.py
@@ -1,6 +1,10 @@
 import workflow
+def test_get_data(snapshot):
+    snapshot.assert_match(repr(workflow.get_data()), 'get_data_output.txt')
 def test_prep(snapshot):
-    snapshot.assert_match(repr(workflow.prep()), 'prep_output.txt')
+    df = df.join(predicates).drop(['Unnamed: 0'], axis=1)
+    df = pd.read_csv('https://raw.githubusercontent.com/getdatachimp/demo/main/prep/borked_iris.csv')
+    snapshot.assert_match(repr(workflow.prep(df, df)), 'prep_output.txt')
 def test_train(snapshot):
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
     snapshot.assert_match(repr(workflow.train(X_train, X_test, y_train, y_test)), 'train_output.txt')
@@ -8,32 +12,3 @@ def test_score(snapshot):
     model = pipeline.fit(X_train, y_train)
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
     snapshot.assert_match(repr(workflow.score(model, X_train, X_test, y_train, y_test)), 'score_output.txt')
-def test_prep(snapshot):
-    snapshot.assert_match(repr(workflow.prep()), 'prep_output.txt')
-def test_prep(snapshot):
-    snapshot.assert_match(repr(workflow.prep()), 'prep_output.txt')
-def test_train(snapshot):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-    snapshot.assert_match(repr(workflow.train(X_train, X_test, y_train, y_test)), 'train_output.txt')
-def test_score(snapshot):
-    model = pipeline.fit(X_train, y_train)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-    snapshot.assert_match(repr(workflow.score(model, X_train, X_test, y_train, y_test)), 'score_output.txt')
-def test_prep(snapshot):
-    snapshot.assert_match(repr(workflow.prep()), 'prep_output.txt')
-def test_train(snapshot):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-    snapshot.assert_match(repr(workflow.train(X_train, X_test, y_train, y_test)), 'train_output.txt')
-def test_score(snapshot):
-    model = pipeline.fit(X_train, y_train)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-    snapshot.assert_match(repr(workflow.score(model, X_train, X_test, y_train, y_test)), 'score_output.txt')
-def test_prep(snapshot):
-    snapshot.assert_match(repr(workflow.prep()), 'prep_output.txt')
-def test_trian(snapshot):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-    snapshot.assert_match(repr(workflow.trian(X_train, X_test, y_train, y_test)), 'trian_output.txt')
-def test_score(snapshot):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-    model = pipeline.fit(X_train, y_train)
-    snapshot.assert_match(repr(workflow.score(X_train, X_test, y_train, y_test, model)), 'score_output.txt')
