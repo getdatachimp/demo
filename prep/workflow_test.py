6ce266c20878c9a1ee5b8fb7b19d68eae63f1aea
Matt Dupree <kmdupr33@gmail.com>
2023-02-10 13:30:23 -0500

    update workflow

--- prep/workflow_test.py
+++ prep/workflow_test.py
@@ -62,3 +62,12 @@ def test_train(snapshot):
     snapshot.assert_match(repr(workflow.train(X_train, X_test, y_train, y_test)), 'train_output.txt')
 def test_get_data(snapshot):
     snapshot.assert_match(repr(workflow.get_data()), 'get_data_output.txt')
+def test_prep(snapshot):
+    df = df.join(predicates).drop(['Unnamed: 0'], axis=1)
+    df = pd.read_csv('https://raw.githubusercontent.com/getdatachimp/demo/main/prep/borked_iris.csv')
+    snapshot.assert_match(repr(workflow.prep(df, df)), 'prep_output.txt')
+def test_get_data(snapshot):
+    snapshot.assert_match(repr(workflow.get_data()), 'get_data_output.txt')
+def test_train(snapshot):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    snapshot.assert_match(repr(workflow.train(X_train, X_test, y_train, y_test)), 'train_output.txt')
